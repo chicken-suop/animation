1ba1060b3bba1b1bf15d9b2a2d51c26a77b4a6a5
Elliot Schep <elliot@desygner.com>
2022-12-12 10:55:24 +0000

    ✨ feat: testing examples

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -1,5 +1,41 @@
-import { PageProps } from "$fresh/server.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { Head } from "$fresh/runtime.ts";
 
-export default function Greet(props: PageProps) {
-  return <div>Hello {props.params.name}</div>;
+interface User {
+  login: string;
+  name: string;
+  avatar_url: string;
+}
+
+export const handler: Handlers<User | null> = {
+  async GET(_, ctx) {
+    const { name } = ctx.params;
+    const resp = await fetch(`https://api.github.com/users/${name}`);
+    if (resp.status === 404) {
+      return ctx.render(null);
+    }
+    const user: User = await resp.json();
+    return ctx.render(user);
+  },
+};
+
+export default function Page({ data }: PageProps<User | null>) {
+  if (!data) {
+    return <h1>User not found</h1>;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{data.name}</title>
+        <link rel="icon" type="image/png" href={data.avatar_url} />
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md">
+        <img src={data.avatar_url} class="w-32 h-32" />
+        <h1 class="flex-grow-1 font-bold text-xl">
+          {data.name} ({data.login})
+        </h1>
+      </div>
+    </>
+  );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,10 @@
 import { Head } from "$fresh/runtime.ts";
 import Counter from "../islands/Counter.tsx";
+import Countdown from "../islands/Countdown.tsx";
 
 export default function Home() {
+  const date = new Date();
+  date.setSeconds(date.getSeconds() + 10);
   return (
     <>
       <Head>
@@ -13,6 +16,9 @@ export default function Home() {
           class="w-32 h-32"
           alt="the fresh logo: a sliced lemon dripping with juice"
         />
+        <p>
+          The big event is happening <Countdown target={date.toISOString()} />.
+        </p>
         <p class="my-6">
           Welcome to `fresh`. Try updating this message in the
           ./routes/index.tsx file, and refresh.
